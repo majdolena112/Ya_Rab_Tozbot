4f2f4c460110ab3ddf753f284a931a74fd13d417
majdo <majdo@192.168.1.111>
2019-02-11 07:55:19 -0800

    yaaaay

---
+++ src/classaaa.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class classaaa {
+	
+	public classaaa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String [] args) {
+		System.out.println("ENTER YOUR NAME:");
+		Scanner input = new Scanner(System.in);
+		String s= input.nextLine();
+		System.out.println("Welcome  "+s);
+	}
+}
