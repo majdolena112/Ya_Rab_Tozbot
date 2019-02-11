be0a24ebae011880ee5f91216b75602a9f749093
majdo <majdo@192.168.1.111>
2019-02-11 08:06:49 -0800

    yaaaay22

--- src/classaaa.java
+++ src/classaaa.java
@@ -8,9 +8,13 @@ public class classaaa {
 	}
 
 	public static void main(String [] args) {
+		
 		System.out.println("ENTER YOUR NAME:");
+		
 		Scanner input = new Scanner(System.in);
+		
 		String s= input.nextLine();
+		
 		System.out.println("Welcome  "+s);
 	}
 }
